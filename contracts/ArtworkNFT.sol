2990d84848415d8b3ab87c6304ea2a2734137970
vitonft <vitonft@163.com>
2021-06-08 17:54:52 +0800

    add pixel overlap check

--- contracts/ArtworkNFT.sol
+++ contracts/ArtworkNFT.sol
@@ -5,42 +5,117 @@ import "./libraries/TransferHelper.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Pausable.sol";
 import "openzeppelin-solidity/contracts/access/AccessControl.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract ArtworkNFT is ERC721Pausable, AccessControl, Ownable {
+    using SafeMath for uint256;
+
+    uint256 constant public minimumStep = 10;
+
+    struct Liberty {
+        uint256 index;
+        uint256 startX;
+        uint256 startY;
+        uint256 xLength;
+        uint256 yLength;
+    }
     bytes32 public constant UPDATE_TOKEN_URI_ROLE = keccak256('UPDATE_TOKEN_URI_ROLE');
     bytes32 public constant PAUSED_ROLE = keccak256('PAUSED_ROLE');
     uint256 public nextTokenId = 1;
     address public mintFeeAddr;
     uint256 public mintFeeAmount;
+    address public modifyFeeAddr;
+    uint256 public modifyFeeAmount;
+
+    address public mintFeeTokenAddr;
+    address public modifyFeeTokenAddr;
+    mapping(uint256 => bool[100][100]) public libertyMap;
+    mapping(uint256 => bool) public enableIndexMap;
+    mapping(uint256 => Liberty) public nftLibertyMap;
+
     event Burn(address indexed sender, uint256 tokenId);
     event MintFeeAddressTransferred(address indexed previousOwner, address indexed newOwner);
     event SetMintFeeAmount(address indexed seller, uint256 oldMintFeeAmount, uint256 newMintFeeAmount);
+    event SetModifyFeeAmount(address indexed seller, uint256 oldModifyFeeAmount, uint256 newModifyFeeAmount);
 
     constructor(
         string memory name,
         string memory symbol,
+        address _mintFeeTokenAddr,
+        address _modifyFeeTokenAddr,
         address _mintFeeAddr,
-        uint256 _mintFeeAmount
+        uint256 _mintFeeAmount,
+        address _modifyFeeAddr,
+        uint256 _modifyFeeAmount
     ) public ERC721(name, symbol) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
         _setupRole(UPDATE_TOKEN_URI_ROLE, _msgSender());
         _setupRole(PAUSED_ROLE, _msgSender());
+        mintFeeTokenAddr = _mintFeeTokenAddr;
+        modifyFeeTokenAddr = _modifyFeeTokenAddr;
         mintFeeAddr = _mintFeeAddr;
         mintFeeAmount = _mintFeeAmount;
+        modifyFeeAddr = _modifyFeeAddr;
+        modifyFeeAmount = _modifyFeeAmount;
         emit MintFeeAddressTransferred(address(0), mintFeeAddr);
         emit SetMintFeeAmount(_msgSender(), 0, mintFeeAmount);
     }
 
     receive() external payable {}
 
-    function mint(address to, string memory _tokenURI) public payable returns (uint256 tokenId) {
-        require(msg.value >= mintFeeAmount, 'msg value too low');
-        TransferHelper.safeTransferETH(mintFeeAddr, mintFeeAmount);
+    function enableIndex(uint256 index) public onlyOwner {
+        require(index==0 || (index>0 && enableIndexMap[index-1]), "enable previous one first");
+        enableIndexMap[index] = true;
+    }
+
+    function mint(address to, string memory _tokenURI, uint256 index, uint256 startX, uint256 startY, uint256 xLength, uint256 yLength) public returns (uint256 tokenId) {
+        require(validatePixel(index, startX, startY, xLength, yLength), "invalid pixel");
+        TransferHelper.safeTransferFrom(mintFeeTokenAddr, msg.sender, address(this), mintFeeAmount);
         tokenId = nextTokenId;
         _mint(to, tokenId);
         nextTokenId++;
         _setTokenURI(tokenId, _tokenURI);
-        if (msg.value > mintFeeAmount) TransferHelper.safeTransferETH(msg.sender, msg.value - mintFeeAmount);
+        Liberty memory liberty = Liberty({
+            index:   index,
+            startX:  startX,
+            startY:  startY,
+            xLength: xLength,
+            yLength: yLength
+        });
+        nftLibertyMap[tokenId] = liberty;
+        markAsUsed(index, startX, startY, xLength, yLength);
+    }
+
+    function markAsUsed(uint256 index, uint256 startX, uint256 startY, uint256 xLength, uint256 yLength) internal {
+        for (uint256 x = startX; x< startX.add(xLength);x=x+minimumStep) {
+            for (uint256 y = startY; y< startY.add(yLength);y=y+minimumStep) {
+                bool[100][100] storage pixelArray = libertyMap[index];
+                pixelArray[x][y] = true;
+            }
+        }
+    }
+
+    function validatePixel(uint256 index, uint256 startX, uint256 startY, uint256 xLength, uint256 yLength) public view returns(bool) {
+        require(enableIndexMap[index], "index is not enabled yet");
+        require(startX!=0 && startY!=0 && xLength!=0 && yLength!=0,"should not be 0");
+
+        require(startX<1000 && startX.add(xLength)<=1000,"x exceed boundary");
+        require(startY<1000 && startY.add(yLength)<=1000,"y exceed boundary");
+
+        require(startX.mod(minimumStep)==0 &&
+            startY.mod(minimumStep)==0 &&
+            xLength.mod(xLength)==0 &&
+            yLength.mod(minimumStep)==0,"pixel should align to 10");
+
+        for (uint256 x = startX; x< startX.add(xLength);x=x+minimumStep) {
+            for (uint256 y = startY; y< startY.add(yLength);y=y+minimumStep) {
+                bool[100][100] memory pixelArray = libertyMap[index];
+                if (pixelArray[x][y]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     function burn(uint256 tokenId) public {
@@ -55,7 +130,7 @@ contract ArtworkNFT is ERC721Pausable, AccessControl, Ownable {
     }
 
     function setTokenURI(uint256 tokenId, string memory tokenURI) public {
-        require(hasRole(UPDATE_TOKEN_URI_ROLE, _msgSender()), 'Must have update token uri role');
+        require(_isApprovedOrOwner(_msgSender(), tokenId), 'caller is not owner nor approved');
         _setTokenURI(tokenId, tokenURI);
     }
 
@@ -79,5 +154,11 @@ contract ArtworkNFT is ERC721Pausable, AccessControl, Ownable {
         require(mintFeeAmount != _mintFeeAmount, 'Not need update');
         emit SetMintFeeAmount(_msgSender(), mintFeeAmount, _mintFeeAmount);
         mintFeeAmount = _mintFeeAmount;
+    }
+
+    function setModifyFeeAmount(uint256 _modifyFeeAmount) public onlyOwner {
+        require(modifyFeeAmount != _modifyFeeAmount, 'Not need update');
+        emit SetMintFeeAmount(_msgSender(), modifyFeeAmount, _modifyFeeAmount);
+        modifyFeeAmount = _modifyFeeAmount;
     }
 }

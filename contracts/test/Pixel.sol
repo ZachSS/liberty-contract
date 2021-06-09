26b2fd80f7b8a82d3dd0346f964859fd8056f405
vitonft <vitonft@163.com>
2021-06-09 12:16:06 +0800

    add test token

---
+++ contracts/test/Pixel.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.6.12;
+
+import "../libraries/BEP20.sol";
+
+contract PixelToken is BEP20 {
+  constructor() public BEP20("Pixel Token", "Pixel") {
+    super._mint(_msgSender(), 1e8*1e18);
+  }
+}
